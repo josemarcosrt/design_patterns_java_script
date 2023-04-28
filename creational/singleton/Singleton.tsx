19ed72402aedb143c174686814ec375efaa69a12
josemarcosrt@yahoo.com.br <jose.teixeira@MacBook-Pro-de-Jose.local>
2023-04-28 16:34:35 -0300

    first commit by the singleton code

---
+++ creational/singleton/Singleton.tsx
@@ -0,0 +1,30 @@
+class SingletonDB {
+    private static instance: SingletonDB;
+
+    private constructor() { }
+
+    public static getInstance(): SingletonDB {
+        if (!SingletonDB.instance) {
+            SingletonDB.instance = new SingletonDB();
+        }
+
+        return SingletonDB.instance;
+    }
+
+    public someBusinessLogic() {
+        // put in here, Business rules whatever you want, and be happy 
+    }
+}
+
+function clientCode() {
+    const s1 = SingletonDB.getInstance();
+    const s2 = SingletonDB.getInstance();
+
+    if (s1 === s2) {
+        console.log('Singleton works, both variables contain the same instance.');
+    } else {
+        console.log('Singleton failed, variables contain different instances.');
+    }
+}
+
+clientCode();
