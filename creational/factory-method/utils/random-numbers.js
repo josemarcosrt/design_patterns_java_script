3a7ef00169126548f3795d35d9d69cc6c56fef66
José Marcos Ramos Teixeira <josemarcosrt@yahoo.com.br>
2023-07-01 01:18:21 -0300

    factory-method arcteture

--- creational/factory-method/utils/random-numbers.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.randomNumbers = void 0;
-function randomNumbers(length) {
-    return Math.floor(Math.random() * length);
-}
-exports.randomNumbers = randomNumbers;
